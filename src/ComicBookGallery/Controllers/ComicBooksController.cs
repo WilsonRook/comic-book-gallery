168b7c183edcaa3d09e61034bb0685958ba4fe6e
Wilson <wilson.rook@gmail.com>
2016-10-24 19:28:04 -0400

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,16 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
 
-            return Content("Hello from the comic books controller!");
+            return View();
+            
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            //{
+                //return Redirect("/");
+                //return new RedirectResult("/");
+            //}
+
+            //return Content("Hello from the comic books controller!");
             //return new ContentResult()
             //{
             //    Content = "Hello from the comic books controller!"
